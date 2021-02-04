3b4ae00b1c38618a6548a8c503301a5d4c1b4578
Hugo Kempfer <hugkempf@gmail.com>
2021-02-04 17:42:32 +0900

    playground: feat -> add futur playground running under Vue3 and WASM

---
+++ rust-nn-playground/src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import * as Rustnn from "rust-nn";
+
+//Hack for globally providing the type of the rustnn module
+export const RustnnType: typeof Rustnn = null;
+
+import("rust-nn").then(wasm => {
+  const app = createApp(App).use(router);
+  app.provide("rustnn", wasm);
+  app.mount("#app");
+});
---
+++ rust-nn-playground/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    {{ wow }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { inject } from "vue";
+import { RustnnType } from "@/main";
+
+@Options({
+  components: {}
+})
+export default class Home extends Vue {
+  wow = 0;
+  rustnn: typeof RustnnType = inject("rustnn") as typeof RustnnType;
+  network = this.rustnn.create_network(28 * 28, 10, 200, 0.1);
+
+  created(): void {
+    this.wow = this.rustnn.damn();
+    console.log(this.network);
+  }
+}
+</script>
